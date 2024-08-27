3cff0a7aabca3862ca915213f3b51c2c4728f5d2
TanDat883 <21004435.dat@student.iuh.edu.vn>
2024-08-27 23:14:37 +0700

    first commit

--- src/iuh/fit/se/Rectangle.java
+++ src/iuh/fit/se/Rectangle.java
@@ -1,17 +1,67 @@
 /*
  * @(#) $(NAME).java    1.0     8/23/2024
- * 
+ *
  * Copyright (c) 2024 IUH. All rights reserved.
  */
- 
+
 package iuh.fit.se;
 
 /*
  * @description
  * @author: Tran Tan Dat
  * @version: 1.0
- * @created: 23-August-2024 1:35 PM     
+ * @created: 23-August-2024 1:35 PM
  */
 
 public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle() {
+        this.width = 1.0;
+        this.height = 1.0;
+    }
+
+    public Rectangle(double width, double height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Length and width must be greater than 0");
+        }
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (this.width + this.height);
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            throw new IllegalArgumentException("Width must be greater than 0");
+        }
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            throw new IllegalArgumentException("Height must be greater than 0");
+        }
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle[width=" + this.width + ", height=" + this.height + "]";
+    }
 }
--- src/iuh/fit/se/TestRectangle.java
+++ src/iuh/fit/se/TestRectangle.java
@@ -1,17 +1,40 @@
 /*
  * @(#) $(NAME).java    1.0     8/23/2024
- * 
+ *
  * Copyright (c) 2024 IUH. All rights reserved.
  */
- 
+
 package iuh.fit.se;
 
 /*
  * @description
  * @author: Tran Tan Dat
  * @version: 1.0
- * @created: 23-August-2024 1:37 PM     
+ * @created: 23-August-2024 1:37 PM
  */
 
 public class TestRectangle {
+    public static void main(String[] args) {
+        Rectangle rectangle1 = new Rectangle();
+        System.out.println("Rectangle 1");
+        System.out.println("Width: " + rectangle1.getWidth());
+        System.out.println("Height: " + rectangle1.getHeight());
+        System.out.println("Area: " + rectangle1.getArea());
+        System.out.println("Perimeter: " + rectangle1.getPerimeter());
+
+        Rectangle rectangle2 = new Rectangle(-2.0, 3.0);
+        System.out.println("\nRectangle 2");
+        System.out.println("Width: " + rectangle2.getWidth());
+        System.out.println("Height: " + rectangle2.getHeight());
+        System.out.println("Area: " + rectangle2.getArea());
+        System.out.println("Perimeter: " + rectangle2.getPerimeter());
+
+        rectangle2.setWidth(4.0);
+        rectangle2.setHeight(-5.0);
+        System.out.println("\nRectangle 2 after set width and height");
+        System.out.println("Width: " + rectangle2.getWidth());
+        System.out.println("Height: " + rectangle2.getHeight());
+        System.out.println("Area: " + rectangle2.getArea());
+        System.out.println("Perimeter: " + rectangle2.getPerimeter());
+    }
 }
