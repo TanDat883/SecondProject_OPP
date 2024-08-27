3c646e8cc5a46b8a0b25b0d0bbad0b54bc0eecbb
TanDat883 <21004435.dat@student.iuh.edu.vn>
2024-08-27 23:10:12 +0700

    first commit

---
+++ src/iuh/fit/se/Rectangle.java
@@ -0,0 +1,17 @@
+/*
+ * @(#) $(NAME).java    1.0     8/23/2024
+ * 
+ * Copyright (c) 2024 IUH. All rights reserved.
+ */
+ 
+package iuh.fit.se;
+
+/*
+ * @description
+ * @author: Tran Tan Dat
+ * @version: 1.0
+ * @created: 23-August-2024 1:35 PM     
+ */
+
+public class Rectangle {
+}
